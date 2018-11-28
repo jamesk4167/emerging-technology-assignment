7c6ebd03547d676af6f6bdb69df15f4db1c67718
G00341457 <G00341457@gmit.ie>
2018-11-28 20:23:12 +0000

    trying to fix digitrecognition script

--- Digit_Recognition.py
+++ Digit_Recognition.py
@@ -3,72 +3,50 @@ import cv2
 import gzip
 import keras as kr
 import sklearn.preprocessing as pre
-
-def Reshape() :
-    #import the image
-    img = cv2.imread("5.jpg")
-
-    #Convert the image to grey scale(needs to be done as Mnist images are in greyscale)
-    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    #Check to see if the image has been converted
-    print(type(gray_image))
-
-    #Resize the image into a 26 by 26 array so it is the same format as our NN data
-    Correct_size_img = cv2.resize(gray_image,(26,26))
-   
-
-    #print(Correct_size_img.shape)
-
+import matplotlib.pyplot as plt
 
 def NeuralNetwork() :
-    #create a sequential model of our neural network
     model = kr.models.Sequential()
 
-
-    model.add = (kr.layers.Dense(units = 512, activation = 'relu', input_dim = 784))
-    model.add = (kr.layers.Dense(units = 512, activation = 'relu'))
-    model.add = (kr.layers.Dense(units = 10, activation = 'softmax'))
-
-    model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-    model.build()
+    model.add(kr.layers.Dense(units = 512, activation = 'relu', input_dim = 784))
+    model.add(kr.layers.Dense(units = 512, activation = 'relu'))
+    model.add(kr.layers.Dense(units=10, activation='softmax'))
 
 
+    model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
 
-    with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
+    with gzip.open('data/train-images-idx3-ubyte.gz') as f:
+       train_img = f.read()
+
+    with gzip.open('data/train-labels-idx1-ubyte.gz') as f:
+       train_lbl = f.read()
+
+
+    train_img = ~np.array(list(train_img[16:])).reshape(60000, 28,28).astype(np.uint8) / 255.0
+    train_lbl = np.array(list(train_lbl[8:])).astype(np.uint8)
+
+    inputs = train_img.reshape(6000, 784)
+
+    encoder = pre.LabelBinarizer()
+    encoder.fit(train_lbl)
+    outputs = encoder.transform(train_lbl)
+
+    model.fit(inputs, outputs, epoch = 2, batch_size = 100)
+
+
+    with gzip.open('data/t10k-images-idx3.ubyte.gz' 'rb') as f:
         test_img = f.read()
-    
 
-    with gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
+    with gzip.open('data/t10k-labels-idx3.ubyte.gz' 'rb') as f:
         test_lbl = f.read()
-    
-    test_images = ~np.array(list(test_img[16:])).reshape(10000, 784).astype(np.uint8) / 255.0
-    test_labels =  np.array(list(test_lbl[ 8:])).astype(np.uint8)
 
 
-    (encoder.inverse_transform(model.predict(test_images)) == test_labels).sum()
+    test_img = ~np.array(list(test_img[16:])).reshape(60000, 28,28).astype(np.uint8) / 255.0
+    test_lbl = np.array(list(test_lbl[8:])).astype(np.uint8)
 
-def readingFiles() :
-    with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
-        train_imgs = f.read()
+    (encoder.inverse_transform(model.predict(test_img)) == test_lbl).sum()
 
-    with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
-        train_lbl = f.read()
-    
-    train_imgs = ~np.array(list(train_imgs[16:])).reshape(60000, 28, 28).astype(np.uint8) /255.0
-    train_lbl = np.array(list(train_lbl [8:])).astype(np.uint8)
+    model.predict(test_img[5:6])
 
-
-def Menu() :
-    print("Please enter the image you want to check")
-
-def main() :
-
-    readingFiles()
-
-
-
-if __name__ == '__main__':
-    main()
-
+    plt.imshow(test_img[5].reshape(28,28), cmap='gray')
