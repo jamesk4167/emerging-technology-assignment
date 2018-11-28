1c682cd7591ec710289b8276af4e8d25d73507d4
G00341457 <G00341457@gmit.ie>
2018-11-28 21:47:25 +0000

    added user functionality to digit script

--- Digit_Recognition.py
+++ Digit_Recognition.py
@@ -4,8 +4,11 @@ import gzip
 import keras as kr
 import sklearn.preprocessing as pre
 import matplotlib.pyplot as plt
+import PIL
+from PIL import Image
+import os.path
 
-def NeuralNetwork() :
+def NeuralNetwork(userImage) :
     model = kr.models.Sequential()
 
     model.add(kr.layers.Dense(units = 512, activation = 'relu', input_dim = 784))
@@ -22,6 +25,11 @@ def NeuralNetwork() :
     with gzip.open('data/train-labels-idx1-ubyte.gz') as f:
        train_lbl = f.read()
 
+    with gzip.open('data/t10k-images-idx3.ubyte.gz' 'rb') as f:
+        test_img = f.read()
+
+    with gzip.open('data/t10k-labels-idx3.ubyte.gz' 'rb') as f:
+        test_lbl = f.read()
 
     train_img = ~np.array(list(train_img[16:])).reshape(60000, 28,28).astype(np.uint8) / 255.0
     train_lbl = np.array(list(train_lbl[8:])).astype(np.uint8)
@@ -33,20 +41,34 @@ def NeuralNetwork() :
     outputs = encoder.transform(train_lbl)
 
     model.fit(inputs, outputs, epoch = 2, batch_size = 100)
+   
+    print(encoder.inverse_transform(model.predict(userImage)))
 
 
-    with gzip.open('data/t10k-images-idx3.ubyte.gz' 'rb') as f:
-        test_img = f.read()
+def userImage(userImg):
+    #read in the image in greyscale
+    image = cv2.imread(userImg, 0)
+    print(image.shape)
+    #resize the image to 28 by 28 format as this is what our neural network understands
+    correct_img = cv2.resize(image,(28,28))
+    print(correct_img.shape)
+    img = prepareImage(correct_img)
+    NeuralNetwork(img)
 
-    with gzip.open('data/t10k-labels-idx3.ubyte.gz' 'rb') as f:
-        test_lbl = f.read()
+def prepareImage(correct_image):
+    #get all of the pixel values for the data
+    imageData = list(correct_image.get_data())
+    #next need to normalise the data as our Neural network has also been normalized multiplying by (1/255) should do
+    imageData = [(255 - i) * 1/255.0 for i in imageData]
 
+    #finally rebuild the image
+    imageData.np.array(list(imageData)).reshape(1, 784)
+    #return the image
+    return imageData
 
-    test_img = ~np.array(list(test_img[16:])).reshape(60000, 28,28).astype(np.uint8) / 255.0
-    test_lbl = np.array(list(test_lbl[8:])).astype(np.uint8)
+#main function    
+def main():
+   userImage('5.jpg')
 
-    (encoder.inverse_transform(model.predict(test_img)) == test_lbl).sum()
-
-    model.predict(test_img[5:6])
-
-    plt.imshow(test_img[5].reshape(28,28), cmap='gray')
+if __name__ == '__main__':
+    main()
