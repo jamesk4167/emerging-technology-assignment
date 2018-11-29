136cf97fb41ae755f19867e1926d9a11f87b8644
G00341457 <G00341457@gmit.ie>
2018-11-29 15:13:16 +0000

    got digit recognition working with Neural NEtwork

--- Digit_Recognition.py
+++ Digit_Recognition.py
@@ -9,6 +9,7 @@ from PIL import Image
 import os.path
 
 def NeuralNetwork(userImage) :
+    #create a sequential model with keras
     model = kr.models.Sequential()
 
     model.add(kr.layers.Dense(units = 512, activation = 'relu', input_dim = 784))
@@ -25,50 +26,56 @@ def NeuralNetwork(userImage) :
     with gzip.open('data/train-labels-idx1-ubyte.gz') as f:
        train_lbl = f.read()
 
-    with gzip.open('data/t10k-images-idx3.ubyte.gz' 'rb') as f:
-        test_img = f.read()
-
-    with gzip.open('data/t10k-labels-idx3.ubyte.gz' 'rb') as f:
-        test_lbl = f.read()
-
     train_img = ~np.array(list(train_img[16:])).reshape(60000, 28,28).astype(np.uint8) / 255.0
     train_lbl = np.array(list(train_lbl[8:])).astype(np.uint8)
 
-    inputs = train_img.reshape(6000, 784)
+    inputs = train_img.reshape(60000, 784)
 
     encoder = pre.LabelBinarizer()
     encoder.fit(train_lbl)
     outputs = encoder.transform(train_lbl)
-
-    model.fit(inputs, outputs, epoch = 2, batch_size = 100)
+    print("training Neural Network")
+    model.fit(inputs, outputs, epoch = 10, batch_size = 100)
    
     print(encoder.inverse_transform(model.predict(userImage)))
 
 
 def userImage(userImg):
     #read in the image in greyscale
-    image = cv2.imread(userImg, 0)
-    print(image.shape)
+    image = Image.open(userImg)
+    greyImage = image.convert('L')
+    #imshow
+    #print(image.shape)
     #resize the image to 28 by 28 format as this is what our neural network understands
-    correct_img = cv2.resize(image,(28,28))
-    print(correct_img.shape)
-    img = prepareImage(correct_img)
-    NeuralNetwork(img)
+    ResizedImage = greyImage.resize((28,28),Image.BICUBIC)
+    #print(correct_img.shape)
 
-def prepareImage(correct_image):
+    preparedImage = prepareImage(ResizedImage)
+    NeuralNetwork(preparedImage)
+
+def prepareImage(img):
     #get all of the pixel values for the data
-    imageData = list(correct_image.get_data())
+    imageData = list(img.getdata())
     #next need to normalise the data as our Neural network has also been normalized multiplying by (1/255) should do
-    imageData = [(255 - i) * 1/255.0 for i in imageData]
+    #for i in imageData:
+    imageData = [(255 - i) * 1.0 / 255.0 for i in imageData]
+    
 
     #finally rebuild the image
-    imageData.np.array(list(imageData)).reshape(1, 784)
-    #return the image
-    return imageData
+    RebuiltImg = np.array(list(imageData)).reshape(1,784)
+    #used this print statement to make sure the image was resized properly
+    #print(RebuiltImg)
+    ## Return the image data
+    return RebuiltImg
+
+
+def Menu():
+    imageDir = input("please enter the path to the image you want to check")
+    userImage(imageDir)
 
 #main function    
 def main():
-   userImage('5.jpg')
+   Menu()
 
 if __name__ == '__main__':
     main()
