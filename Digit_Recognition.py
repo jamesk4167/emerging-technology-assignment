89ece57bdc7df1b01f31fe8954441903c9f6942b
G00341457 <G00341457@gmit.ie>
2018-11-30 16:25:41 +0000

    Got the digit Recognition script working

--- Digit_Recognition.py
+++ Digit_Recognition.py
@@ -6,10 +6,12 @@ import sklearn.preprocessing as pre
 import matplotlib.pyplot as plt
 import PIL
 from PIL import Image
+import os
 import os.path
 from keras.models import model_from_json
 
 def NeuralNetwork() :
+    global encoder
     #create a sequential model with keras
     model = kr.models.Sequential()
 
@@ -28,7 +30,7 @@ def NeuralNetwork() :
     with gzip.open('data/train-labels-idx1-ubyte.gz') as f:
        train_lbl = f.read()
     #convert to a numpy array and divide by 255 as RGB values are between 255 and 0
-    train_img = ~np.array(list(train_img[16:])).reshape(60000, 28,28).astype(np.uint8) / 255.0
+    train_img = np.array(list(train_img[16:])).reshape(60000, 28,28).astype(np.uint8) / 255.0
     train_lbl = np.array(list(train_lbl[8:])).astype(np.uint8)
 
     inputs = train_img.reshape(60000, 784)
@@ -36,9 +38,9 @@ def NeuralNetwork() :
     encoder = pre.LabelBinarizer()
     encoder.fit(train_lbl)
     outputs = encoder.transform(train_lbl)
-    print(train_lbl[0], outputs[0])
-    print("training Neural Network")
-    model.fit(inputs, outputs, epochs=100, batch_size = 100)
+    #print(train_lbl[0], outputs[0])
+
+    model.fit(inputs, outputs, epochs=10, batch_size = 100)
     #save the model to a json file
     model_json = model.to_json()
     with open("model.json", "w") as json_file:
@@ -46,22 +48,48 @@ def NeuralNetwork() :
    
     #save the weights 
     model.save_weights("model.h5")
-    print("Saved model to disk")
-    #print(encoder.inverse_transform(model.predict(userImage)))
+    print("Saved the model to disk")
+    #test if the neural network works
+    with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
+
+        test_img = f.read()
+
+    with gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
+        test_lbl = f.read()
+    
+    test_img = np.array(list(test_img[16:])).reshape(10000, 784).astype(np.uint8) / 255.0
+    test_lbl = np.array(list(test_lbl[ 8:])).astype(np.uint8)
+
+    #encoder.inverse_transform(model.predict(test_img)) == test_lbl.sum()
+
+    #print(model.predict_classes(userImg))
+
+    
+    plt.imshow(test_img[5].reshape(28, 28), cmap='gray')
+    Menu()
 
 
 def TestAgainstUserImage(userImg):
-    json_file = open('model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    #load weights into the new model
-    loaded_model.load_weights("model.h5")
-    print("loaded model from disk")
-    #loaded_model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+    if(os.path.isfile("model.json")) :
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        #load weights into the new model
+        loaded_model.load_weights("model.h5")
+        print("loaded model from disk")
+        model = loaded_model
+        #loaded_model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+        prediction = model.predict(np.array(userImg, dtype=float))
+
+        print("Predicted: ", prediction)
+        print("The number you have entered is :")
+        #encoder = pre.LabelBinarizer()
+        #print(encoder.inverse_transform(loaded_model.predict_classes(userImg)))
+        print(str(loaded_model.predict_classes(userImg)))
+    else :
+        NeuralNetwork()
     
-    print("The number you have entered is :")
-    print(str(loaded_model.predict_classes(userImg)))
 
 def userImage(userImg):
     #read in the image
@@ -94,8 +122,14 @@ def prepareImage(img):
 
 
 def Menu():
+    #for this program to work I have provided some sample images inside of the folder that can be used eg 5.jpg
     imageDir = input("please enter the path to the image you want to check: ")
-    userImage(imageDir)
+    try:
+       userImage(imageDir) 
+    except FileNotFoundError:
+        print("Sorry this file does not exist, try using 5.jpg or 4.jpg which I have provided for you")
+        print("=========================================================")
+        Menu()
 
 #main function    
 def main():
